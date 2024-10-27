d033b675fcf29bb13807b8a273262c15de8cb215
Korytin Nikita <korytin.nikita@gmail.com>
2024-10-27 21:15:14 +0500

    - Replace test project

---
+++ DockerComposeTesting.Tests/EmptyTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DockerComposeTesting.Tests
+{
+    public class EmptyTest
+    {
+        [Fact]
+        public void Test() => true.Should().BeTrue();
+        public Task TestAsync()
+        {
+            true.Should().BeTrue();
+            return Task.CompletedTask;
+        }
+    }
+}
